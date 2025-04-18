e6ca17fbf14ae16b9ab671806ac08b4f53e1e55d
Adib Khan <adeebzkhan9@gmail.com>
2025-04-18 12:09:25 +0530

    Adib | feat(Socket): Created the SocketContextProvider

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import StackNavigator from './navigation/StackNavigator';
 import {AuthProvider} from './AuthContext';
+import {SocketContextProvider} from './SocketContext';
 
 function App() {
   // mongodb+srv://adibkhan8180:<db_password>@cluster0.rkgtbq9.mongodb.net/
   return (
     <AuthProvider>
-      <StackNavigator />
+      <SocketContextProvider>
+        <StackNavigator />
+      </SocketContextProvider>
     </AuthProvider>
   );
 }
--- store/constant.js
+++ store/constant.js
@@ -1,1 +1,2 @@
 export const BASE_URL = 'http://192.168.1.92:4000';
+export const SOCKET_URL = 'http://192.168.1.92:3000';
