84e0b5e1d52232c89ee97e4122491a6c8ca41a45
Adib Khan <adeebzkhan9@gmail.com>
2025-04-23 22:38:07 +0530

    Adib | Fix(Icons): Fixed the icons error

--- store/constant.js
+++ store/constant.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://192.168.1.92:4000';
-export const SOCKET_URL = 'http://192.168.1.92:3000';
+export const BASE_URL = 'http://192.168.29.243:4000';
+export const SOCKET_URL = 'http://192.168.29.243:3000';
