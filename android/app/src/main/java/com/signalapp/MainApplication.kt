f6f71c7d08e22df259d34bd0cb2c838ef835d190
Adib Khan <adeebzkhan9@gmail.com>
2025-05-15 18:49:42 +0530

    Adib | feat(QuickConnect): signalapp to quickconnet

--- android/app/src/main/java/com/signalapp/MainApplication.kt
+++ android/app/src/main/java/com/signalapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.signalapp
+package com.quickconnect
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/signalapp/AppDelegate.swift
+++ ios/signalapp/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "signalapp",
+      withModuleName: "quickconnect",
       in: window,
       launchOptions: launchOptions
     )
